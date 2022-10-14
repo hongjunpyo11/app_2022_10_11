11c3a9c8688d1c84c9b42b9d7205bcd1e03fd6c5
hongjunpyo11 <jpyo98@naver.com>
2022-10-14 15:55:19 +0900

    30강, RebateOrderItem 목록 페이지 구현

--- src/main/java/com/ll/exam/app__2022_10_11/app/util/Ut.java
+++ src/main/java/com/ll/exam/app__2022_10_11/app/util/Ut.java
@@ -13,6 +13,7 @@ import java.net.URLEncoder;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -40,6 +41,7 @@ public class Ut {
             return parse("yyyy-MM-dd HH:mm:ss.SSSSSS", dateText);
         }
     }
+
     private static ObjectMapper getObjectMapper() {
         return (ObjectMapper) AppConfig.getContext().getBean("objectMapper");
     }
@@ -118,6 +120,10 @@ public class Ut {
                 url += "?";
             }
 
+            if ( url.endsWith("?") == false && url.endsWith("&") == false ) {
+                url += "&";
+            }
+
             url += paramName + "=" + encode(paramValue);
 
             return url;
@@ -151,6 +157,25 @@ public class Ut {
             } catch (UnsupportedEncodingException e) {
                 return str;
             }
+        }
+
+        public static String getQueryParamValue(String url, String paramName, String defaultValue) {
+            String[] urlBits = url.split("\\?", 2);
+
+            if (urlBits.length == 1) {
+                return defaultValue;
+            }
+
+            urlBits = urlBits[1].split("&");
+
+            String param = Arrays.stream(urlBits)
+                    .filter(s -> s.startsWith(paramName + "="))
+                    .findAny()
+                    .orElse(paramName + "=" + defaultValue);
+
+            String value = param.split("=", 2)[1].trim();
+
+            return value.length() > 0 ? value : defaultValue;
         }
     }
 }
